80f4cfe076be3647aa1e0bbea9ac2d461f723899
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-04-19 22:11:42 +0900

    445e1f0a4..b2c3d5f  auth -> auth

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -3,9 +3,6 @@ import BlameSummary from "../../components/blame/BlameSummary";
 import SherrifSummary from "../../components/sheriff/SheriffSummary";
 import TrialSummary from "../../components/trial/TrailSummary";
 import UserDetail from "../../components/userDetail/UserDetail";
-import { useDispatch, useSelector } from "react-redux";
-import api from "../../shared/api";
-import { loginUser, UserState } from "../../store/slice/userSlice";
 
 const Main = () => {
     return (
