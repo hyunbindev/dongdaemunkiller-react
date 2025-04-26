517f61bb41584713a7126dcf5a090593557d8440
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-04-26 11:45:45 +0900

    자기자신 저격금지지

--- src/components/blame/BlameTargetMember.tsx
+++ src/components/blame/BlameTargetMember.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import { MemberSimple } from "../../data/MemberInterface";
 import style from './blameStyle.module.css'
+import { useSelector } from "react-redux";
 interface BlameTargetMemberProps{
     member?:MemberSimple;
     submitHandler:(targetUuid?:string)=>void;
 }
 
 const BlameTargetMember:React.FC<BlameTargetMemberProps> = ({member, submitHandler})=>{
-
+    const userUuid = useSelector((state: any) => state.user.uuid);
     const confirmTarget = () => {
+        if(member?.uuid === userUuid){
+            alert("자기 자신을 저격할 수 없습니다.");
+            return;
+        }
         if(confirm(`${member?.name}님을 저격하시겠습니까?`)){
             submitHandler(member?.uuid);
         }
