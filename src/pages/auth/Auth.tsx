8423c96de376a2e7ae2f1d14db3ec47942f2f453
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-05-27 23:01:25 +0900

    카카오 인앱 브라우저 리다이렉션 수정정

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -7,6 +7,8 @@ const Auth = () => {
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    // 쿠키에서 redirectPath를 가져오는 함수
+    // 오류 추정으로 인하여 deprecate
     function getCookie(name:string) {
       const cookies = document.cookie.split(';').map(c => c.trim());
       for (let cookie of cookies) {
@@ -15,7 +17,16 @@ const Auth = () => {
         }
       }
       return null;
-    } 
+    }
+    // sessionStrage 이용하여 브라우저 닫을 시 초기화 하는 방향
+    function getRedirectPathFromSessionStorage(uri:string) {
+      const path = sessionStorage.getItem('redirectPath');
+      if (path) {
+        sessionStorage.removeItem('redirectPath');
+        return path;
+      }
+      return null;
+    }
     useEffect(()=>{
         const accessToken = searchParams.get('accessToken');
         sessionStorage.setItem('accessToken', accessToken!);
@@ -27,7 +38,7 @@ const Auth = () => {
           })
           .then((response) => {
             dispatch(loginUser(response.data));
-          const savedPath = getCookie('redirectPath');
+          const savedPath = getRedirectPathFromSessionStorage('redirectPath');
           if (savedPath) {
             navigate(savedPath);
             return;
--- src/pages/auth/RedirectionAuth.tsx
+++ src/pages/auth/RedirectionAuth.tsx
@@ -3,9 +3,11 @@ import { useEffect } from "react"
 const RedirectionAuth = () => {
     useEffect(()=>{
         const path = window.location.pathname;
-        document.cookie = `redirectPath=${path}; path=/;`;
+        // 카카오인에 브라우저에서 이상함 쿠키는 제거
+        //document.cookie = `redirectPath=${path}; path=/;`;
+        sessionStorage.setItem('redirectPath', path);
         window.location.href = `/api/oauth2/authorization/kakao`;
     },[])
-    return<></>
+    return<>리다이렉션중...</>
 }
 export default RedirectionAuth
